c982c20a386d21f4b562cb38aefbe023c0fef46d
David Brownell <db@davidbrownell.com>
2024-06-28 07:42:32 -0400

    ✨ Introduced dynamic post generation actions
    Specific actions need to be taken when a project is generated (for example, committing the generated files and pushing them to the origin). However, the actions themselves are dynamic based on the options selected prior to generation (for example, we only need to change git execute permissions on scripts if git is the selected repository tool and the generation is happening on Windows (the permissions can be programmatically added on Linux and MacOS)). 
    
    This change handles those scenarios through the `post_generation_actions.html` file, and postprocess actions (through `__postprocess.py` files) can customize what appears on that page.

--- template/__tools/CopierHelpers.py
+++ template/__tools/CopierHelpers.py
@@ -1,8 +1,11 @@
 import os
+import re
 import shutil
+import textwrap
 
+from enum import auto, Enum
 from pathlib import Path
-from typing import Optional
+from typing import Match, Optional
 
 
 # ----------------------------------------------------------------------
@@ -57,7 +60,7 @@ def MoveFiles(
         (dest_dir / relative_path).mkdir(parents=True, exist_ok=True)
 
         for filename in filenames:
-            if filename == "__postprocess.py":
+            if filename in ["__preprocess.py", "__postprocess.py"]:
                 continue
 
             source_filename = root / filename
@@ -67,10 +70,27 @@ def MoveFiles(
 
 
 # ----------------------------------------------------------------------
+class AugmentFileStyle(Enum):
+    """Controls how content is augmented"""
+
+    # Replace the content or all the content within the tags
+    Replace = auto()
+
+    # Replace the content and remove the tags so that the content is not modified again
+    Finalize = auto()
+
+    # Append the content to the end of the file or end of the content within the tags
+    Append = auto()
+
+    # Prepend the content to the beginning of the file or beginning of the content within the tags
+    Prepend = auto()
+
+
 def AugmentFile(
     filename: Path,
-    tags: Optional[str],
     new_content: str,
+    tag_name: Optional[str],
+    augment_style: AugmentFileStyle = AugmentFileStyle.Append,
 ) -> None:
     if not filename.is_file():
         filename.parent.mkdir(parents=True, exist_ok=True)
@@ -79,10 +99,75 @@ def AugmentFile(
         with filename.open(encoding="utf-8") as f:
             content = f.read()
 
-    if tags:
-        raise Exception("Not implemented yet")
+    if tag_name:
+        if filename.suffix in [
+            ".htm",
+            ".html",
+            ".md",
+        ]:
+            regex = re.compile(
+                rf"""(?#
+                Opening tag     )(?P<opening_tag>\<\!\-\-\s*\[BEGIN\]\s*{re.escape(tag_name)}\s*\-\-\>\s*?\r?\n)(?#
+                Content         )(?P<content>.*?)(?#
+                Closing tag     )(?P<closing_tag>\<\!\-\-\s*\[END\]\s*{re.escape(tag_name)}\s*\-\-\>\s*?\r?\n)(?#
+                )""",
+                re.DOTALL | re.MULTILINE,
+            )
+        else:
+            assert False, filename.suffix  # pragma: no cover
+
+        # ----------------------------------------------------------------------
+        def Sub(
+            match: Match,
+        ) -> str:
+            if augment_style == AugmentFileStyle.Finalize:
+                return new_content
+
+            content = match.group("content")
+
+            if augment_style == AugmentFileStyle.Replace:
+                content = new_content
+            elif augment_style == AugmentFileStyle.Append:
+                content += new_content
+            elif augment_style == AugmentFileStyle.Prepend:
+                content = new_content + content
+            else:
+                assert False, augment_style  # pragma: no cover
+
+            return f"{match.group('opening_tag')}{content}{match.group('closing_tag')}"
+
+        # ----------------------------------------------------------------------
+
+        content = regex.sub(Sub, content)
+
     else:
-        content += new_content
+        if augment_style in [AugmentFileStyle.Replace, AugmentFileStyle.Finalize]:
+            content = new_content
+        elif augment_style == AugmentFileStyle.Append:
+            content += new_content
+        elif augment_style == AugmentFileStyle.Prepend:
+            content = new_content + content
+        else:
+            assert False, augment_style  # pragma: no cover
 
     with filename.open("w", encoding="utf-8") as f:
         f.write(content)
+
+
+# ----------------------------------------------------------------------
+def CreateInstructionContent(
+    title: str,
+    steps_html: str,
+) -> str:
+    return textwrap.dedent(
+        f"""\
+        <details>
+            <summary>
+                <span role="term"><input type="checkbox" id="{title.lower().replace(' ', '-')}">{title}</span>
+            </summary>
+        </details>
+        <div role="definition" class="details-content">
+{steps_html}
+        </div>
+        """,
+    )
