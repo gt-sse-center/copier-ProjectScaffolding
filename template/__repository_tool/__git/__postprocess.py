b3658e03fe0732eec64bf93beae96547b3054613
David Brownell <db@davidbrownell.com>
2024-07-22 09:18:25 -0400

    wip

--- template/__repository_tool/__git/__postprocess.py
+++ template/__repository_tool/__git/__postprocess.py
@@ -9,11 +9,12 @@ from CopierHelpers import *
 
 # ----------------------------------------------------------------------
 def ValidateGitDirectory():
-    git_dir = Path.cwd() / ".git"
-
-    if not git_dir.is_dir():
-        sys.stderr.write(f"\nERROR: A '.git' directory does not exist at '{git_dir}'. Please generate the project into a directory that is an existing git repository.\n\n")
-        sys.exit(-1)
+    return # BugBug
+    # BugBug git_dir = Path.cwd() / ".git"
+    # BugBug
+    # BugBug if not git_dir.is_dir():
+    # BugBug     sys.stderr.write(f"\nERROR: A '.git' directory does not exist at '{git_dir}'. Please generate the project into a directory that is an existing git repository.\n\n")
+    # BugBug     sys.exit(-1)
 
 
 # ----------------------------------------------------------------------
