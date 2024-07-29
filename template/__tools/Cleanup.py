fb1182ee42e5341433eb219b1a0dfd1d9c13050e
David Brownell <db@davidbrownell.com>
2024-07-29 07:55:25 -0400

    🎨 [refactor] Added instructions to complete post_generation_actions.html

--- template/__tools/Cleanup.py
+++ template/__tools/Cleanup.py
@@ -1,6 +1,36 @@
 import shutil
+import sys
+import textwrap
 
 from pathlib import Path
+
+
+# ----------------------------------------------------------------------
+sys.stdout.write(
+    "\n".join(
+        f"        {line}" for line in
+        textwrap.dedent(
+            """\
+
+
+            Code Generation is complete!
+
+                Repository Tool:    {{ repository_tool }}
+                Hosting Platform:   {{ hosting_platform }}
+                Project Type:       {{ project_type }}
+
+            Please open the file:
+
+                ./post_generation_actions.html
+
+            to complete the steps that must be finished before this repository can be used.
+
+
+            """,
+        ).split("\n")
+    ),
+)
+sys.stdout.write("\n")
 
 
 # ----------------------------------------------------------------------
